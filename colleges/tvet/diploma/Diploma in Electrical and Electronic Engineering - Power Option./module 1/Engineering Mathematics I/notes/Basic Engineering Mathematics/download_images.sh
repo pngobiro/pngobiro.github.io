b00a4b9e623ab493a9b0ba879ca9e336ea125f82
pngobiro <pngobiro@gmail.com>
2025-05-06 17:21:09 +0300

    update

--- colleges/tvet/diploma/Diploma in Electrical and Electronic Engineering - Power Option./module 1/Engineering Mathematics I/notes/Basic Engineering Mathematics/download_images.sh
+++ colleges/tvet/diploma/Diploma in Electrical and Electronic Engineering - Power Option./module 1/Engineering Mathematics I/notes/Basic Engineering Mathematics/download_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # --- Configuration ---
-MMD_FILE="analogue-electronics-full.txt" # Input MMD file name
+MMD_FILE="Basic Engineering Mathematics.txt" # Input MMD file name
 OUTPUT_DIR="assets/images" # Output directory for images
 PROJECT_ROOT="/" # Project root relative to script location (if needed)
 
