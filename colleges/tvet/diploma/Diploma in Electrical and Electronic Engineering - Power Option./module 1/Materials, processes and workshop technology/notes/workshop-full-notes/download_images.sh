ccd0e2d0938ffc733758587030aef60f47cf78c2
pngobiro <pngobiro@gmail.com>
2025-05-12 13:30:57 +0300

    Workshop FULL Notes

--- colleges/tvet/diploma/Diploma in Electrical and Electronic Engineering - Power Option./module 1/Materials, processes and workshop technology/notes/workshop-full-notes/download_images.sh
+++ colleges/tvet/diploma/Diploma in Electrical and Electronic Engineering - Power Option./module 1/Materials, processes and workshop technology/notes/workshop-full-notes/download_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # --- Configuration ---
-MMD_FILE="Solar_Installation_Technology.mmd" # Input MMD file name
+MMD_FILE="workshop-full-notes.mmd" # Input MMD file name
 OUTPUT_DIR="assets/images" # Output directory for images
 PROJECT_ROOT="/" # Project root relative to script location (if needed)
 
