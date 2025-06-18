a3edce97d5754686d40512fd65def375d3258eba
pngobiro <pngobiro@gmail.com>
2025-06-18 22:55:13 +0300

    added DPTE Agri

--- colleges/DPTE/Agriculture/Notes/Diploma IN Teacher Education AGRI Notes/download_images.sh
+++ colleges/DPTE/Agriculture/Notes/Diploma IN Teacher Education AGRI Notes/download_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # --- Configuration ---
-MMD_FILE="Diploma IN Teacher Education AGRI Notes.mmd" # Input MMD file name
+MMD_FILE="Diploma IN Teacher Education AGRI Notes.txt" # Input MMD file name
 OUTPUT_DIR="assets/images" # Output directory for images
 PROJECT_ROOT="/" # Project root relative to script location (if needed)
 
