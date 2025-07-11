464308031fe28eba3bb609babf1b6cc055917840
pngobiro <pngobiro@gmail.com>
2025-07-11 07:22:08 +0300

    feat: Upload PDFs to Drive and replace with links

--- upload_pdfs.py
+++ upload_pdfs.py
@@ -10,7 +10,7 @@ from googleapiclient.http import MediaFileUpload
 # --- CONFIGURATION ---
 DRIVE_ROOT_FOLDER_NAME = "GitHub PDF Archive"
 FOLDERS_TO_SCAN = ["cbc", "colleges"]
-CREDENTIALS_FILENAME = "client_secret_640314321701-1r57ln38jedob9djou9di54e9oquao94.apps.googleusercontent.com.json" # Your credentials file
+CREDENTIALS_FILENAME = "client_secret_640314321701-g44blr9idn8j09fljbm17o4llvi581s9.apps.googleusercontent.com.json" # Your credentials file
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 # --- END CONFIGURATION ---
 
