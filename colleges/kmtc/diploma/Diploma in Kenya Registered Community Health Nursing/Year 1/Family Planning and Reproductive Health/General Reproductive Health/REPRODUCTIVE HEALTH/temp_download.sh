72aca7c46169616951b4b72646b65cc08dd5839d
pngobiro <pngobiro@gmail.com>
2025-08-28 14:02:47 +0300

    Reproductive Health

--- colleges/kmtc/diploma/Diploma in Kenya Registered Community Health Nursing/Year 1/Family Planning and Reproductive Health/General Reproductive Health/REPRODUCTIVE HEALTH/temp_download.sh
+++ colleges/kmtc/diploma/Diploma in Kenya Registered Community Health Nursing/Year 1/Family Planning and Reproductive Health/General Reproductive Health/REPRODUCTIVE HEALTH/temp_download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # --- Configuration ---
-MMD_FILE="STI-Guidelines-2021.txt" # Input MMD file name
+MMD_FILE="REPRODUCTIVE HEALTH.txt" # Input MMD file name
 OUTPUT_DIR="assets/images" # Output directory for images
 PROJECT_ROOT="/" # Project root relative to script location (if needed)
 
