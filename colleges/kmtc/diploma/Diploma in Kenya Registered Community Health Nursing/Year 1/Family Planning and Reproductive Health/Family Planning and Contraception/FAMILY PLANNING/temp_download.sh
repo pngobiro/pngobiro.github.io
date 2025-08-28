1b1abe0fe2a85a43de4159bfb2c874f84f394cac
ngobiro <pngobiro@gmail.com>
2025-08-28 19:34:19 +0300

    Family Planning and Reproductive Health

--- colleges/kmtc/diploma/Diploma in Kenya Registered Community Health Nursing/Year 1/Family Planning and Reproductive Health/Family Planning and Contraception/FAMILY PLANNING/temp_download.sh
+++ colleges/kmtc/diploma/Diploma in Kenya Registered Community Health Nursing/Year 1/Family Planning and Reproductive Health/Family Planning and Contraception/FAMILY PLANNING/temp_download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # --- Configuration ---
-MMD_FILE="INTRODUCTION REPRODUCTIVE HEALTH.mmd" # Input MMD file name
+MMD_FILE="FAMILY PLANING & RH.txt" # Input MMD file name
 OUTPUT_DIR="assets/images" # Output directory for images
 PROJECT_ROOT="/" # Project root relative to script location (if needed)
 
